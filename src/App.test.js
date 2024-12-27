636ef67eb80b6e7310fea11edf64524b5f52c559
Matheus <matheusramossilva2003@gmail.com>
2024-12-26 23:49:21 -0300

    g

--- src/App.test.js
+++ src/App.test.js
@@ -3,9 +3,9 @@ import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import App from './App';
+import rootReducer from './reducers';
 
-const reducer = (state = {}, action) => state;
-const store = createStore(reducer);
+const store = createStore(rootReducer);
 
 test('renders learn react link', () => {
   render(
