8d4177b17b1c60dc984c09cf01b4e5093b3ad995
Matheus <matheusramossilva2003@gmail.com>
2024-12-26 23:30:19 -0300

    ah

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer);
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
