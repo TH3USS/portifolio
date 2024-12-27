941f98bf08f04a5c78f6ce9dfafb36f58c151dc1
Matheus <matheusramossilva2003@gmail.com>
2024-12-26 23:54:58 -0300

    vai logo

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import store from './redux/store';
 import App from './App';
-import rootReducer from './reducers';
-
-const store = createStore(rootReducer);
 
 test('renders learn react link', () => {
   render(
