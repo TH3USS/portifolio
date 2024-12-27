3fd617e2fc3633dc6c03f3316a754ae6baee1af0
Matheus <matheusramossilva2003@gmail.com>
2024-12-26 23:37:16 -0300

    f

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { Provider } from 'react-redux';
 import App from './App';
 
-
 test('renders learn react link', () => {
-  render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
+  render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
